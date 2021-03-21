c54dcf32d16ca7491d6c1f7549f02dc7c0da7a39
pavel.usov <pavel.usov@motorsport.com>
2021-03-21 22:50:41 +0500

    The displaying for a configuration form was added #6

--- src/modules/FormGenerator/FormGenerator.module.tsx
+++ src/modules/FormGenerator/FormGenerator.module.tsx
@@ -1,7 +1,8 @@
 import React, { Component }  from "react";
+import Form from "@rjsf/material-ui";
+import { JSONSchema7 } from "json-schema";
 
 import './FromGenerator.scss';
-
 import { FGProvider } from "./context"
 import ConfigurationForm from "./components/ConfigurationForm";
 import configurationExample from "./components/ConfigurationForm/configuration-example.json";
@@ -13,12 +14,8 @@ class FormGenerator extends Component<any, FormGeneratorContext> {
     super(props);
 
     this.setFormState = (props: SetFormProps): void => {
-
-      let form: FormGeneratorState = {};
-
-      if (props?.jsObject?.formTitle) form.formTitle = props.jsObject.formTitle;
-
-      this.setState({ form })
+      const form = props?.jsObject;
+      if (form) this.setState({ form });
     }
 
     this.state = {
@@ -28,13 +25,14 @@ class FormGenerator extends Component<any, FormGeneratorContext> {
   }
 
   render() {
+    const { form } = this.state;
     return (
       <FGProvider
         value={this.state}
       >
         <div className="form-wrapper">
           <ConfigurationForm />
-          {JSON.stringify(this.state.form)}
+          <Form schema={form as JSONSchema7} />
         </div>
       </FGProvider>
     );
